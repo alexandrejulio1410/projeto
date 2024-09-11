d1f1cc2147e6dfffc5d15ef6ed002d9d50b6848b
alexandrejulio1410 <145934588+alexandrejulio1410@users.noreply.github.com>
2024-09-11 23:20:58 +0000

    ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component ({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  nome:string = 'cleyton'
+  idade= 25
+
 
   constructor() {}
 
+  imprime(){
+    return "acorda"
+  }
+  cor = 'red'
+  texto(){
+    if(this.cor=='red'){
+      this.cor='blue'
+    }else{
+      this.cor='red'
+    }
+   
+  }
+  guardaTexto = ''
+
+  escreve(valor:string){
+    console.log(valor)
+    this.guardaTexto= valor
+  }
+  modo(value:string){
+    this.cor = value
+  }
 }
